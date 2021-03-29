2419820ce8fb5e4aac2291e3bb39af26cdca107d
Martín Singla <martinsingla@MacBook-Pro-de-Martin.local>
2021-03-28 20:00:42 -0600

    final version + txt output

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,35 +19,54 @@ with open(csvpath) as csvfile:
     prev_val = "NaN"
     cur_val = []
     prev_day = []
+    dates = []
 
-    dif = []
-
+    #First loop to find Total and Balance
     for i in csvreader:
         month_counter = month_counter + 1
         balance = balance + int(i[1])
 
+        #Create lists with current and previous valies
         cur_val.append(int(i[1]))
         prev_day.append(prev_val)
         prev_val = int(i[1])
+        dates.append(i[0])
     
+    #remove 1st value of the list
     cur_val.pop(0)
     prev_day.pop(0)
+    dates.pop(0)
 
+    #Calculate difference between initial value previous day value
     dif = []
     for j in range(len(cur_val)):
         dif.append(cur_val[j] - prev_day[j])
-
+    
+    #Calculate avg change daily
     averageDailyChange = round(sum(dif) / len(dif), 2)
 
+    #Find max and min values
+    maxV = max(dif)
+    minV = min(dif)
+    maxDate = dates[dif.index(maxV)]
+    minDate = dates[dif.index(minV)]
+
+    #Print summary of results
     print("FINANCIAL ANALYSIS")
     print("-----------------------------")
-    print(f'Total months in period: {month_counter}')
-    print(f'Profit/Losses for perior: ${balance}.-')
+    print(f'Total months: {month_counter}')
+    print(f'Total Profit/Losses: ${balance}.-')
     print(f'Average Daily Change: ${averageDailyChange}')
+    print(f'Greatest Profit Increase: ${maxV}.- (date: {maxDate})')
+    print(f'Greatest Profit Decrease: ${minV}.- (date: {minDate})')
 
-
-
-
-
-
-
+#Export txt file w/ results
+textFile = open("Analysis/Results.txt", mode= 'w')
+textFile.write("FINANCIAL ANALYSIS\n")
+textFile.write("-----------------------------\n")
+textFile.write(f'Total months: {month_counter}\n')
+textFile.write(f'Total Profit/Losses: ${balance}.-\n')
+textFile.write(f'Average Daily Change: ${averageDailyChange}\n')
+textFile.write(f'Greatest Profit Increase: ${maxV}.- (date: {maxDate})\n')
+textFile.write(f'Greatest Profit Decrease: ${minV}.- (date: {minDate})\n')
+textFile.close()
