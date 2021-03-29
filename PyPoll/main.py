911758c5110bd26ed3f4e79519a3c7ffba6c3982
Martín Singla <martinsingla@MacBook-Pro-de-Martin.local>
2021-03-29 11:45:50 -0600

    Added report brakedown by county

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,6 +12,11 @@ with open(csv_path) as csv_file:
     tot_vote_counter = 0
     candidate_list = []
     candidate_votes = []
+    county_list =  []
+    county_tot_votes = []
+
+    full_votes_county_list = []
+    full_votes_cand_list = []
 
     for i in csvreader:
 
@@ -26,6 +31,17 @@ with open(csv_path) as csv_file:
         if (i[2] in candidate_list):
             candidate_votes[candidate_list.index(i[2])] += 1
 
+        #Identify individual counties and tot votes per county
+        if (i[1] not in county_list):
+            county_list.append(i[1])
+            county_tot_votes.append(0)
+        
+        if (i[1] in county_list):
+            county_tot_votes[county_list.index(i[1])] += 1
+
+        full_votes_county_list.append(i[1])
+        full_votes_cand_list.append(i[2])
+    
     #quantify % per candidate and count tot. votes
     candidate_per = []
     for j in range(len(candidate_list)):
@@ -34,8 +50,17 @@ with open(csv_path) as csv_file:
     #Identify Winner
     winner = candidate_list[candidate_votes.index(max(candidate_votes))]
 
-    #Loop process for each county
+    #Make dictionary with counties / candidates / votes per candidate
+    county_candidates = {}
+    for h in county_list:
+        county_candidates[h] = {}
+        for l in candidate_list:
+            county_candidates[h][l] = 0
+    
+    for p in range(len(full_votes_cand_list)):
+        county_candidates[full_votes_county_list[p]][full_votes_cand_list[p]] += 1
 
+    #Print SUMMARY RESULTS
     print("STATE ELECTION RESULTS")
     print("--------------------------")
     print(f"STATE TOTAL VOTES: {tot_vote_counter}")
@@ -45,13 +70,8 @@ with open(csv_path) as csv_file:
     print("-------------------------- \n")
     print(f'STATE WINNER: {winner} \n')
     print("--------------------------")
-    print("RESULTS BY COUNTY \n")
-    print("--------------------------")
 
-    #print stats per county
-
-
-#Write results in text file
+#WRITE TEXT FILE WITH FULL RESULTS
 textFile = open("Analysis/Results.txt", mode= 'w')
 textFile.write("==========================================================\n")
 textFile.write("                  STATE ELECTION RESULTS\n\n")
@@ -64,8 +84,14 @@ textFile.write(f'State Winner: {winner} \n\n\n')
 textFile.write("==========================================================\n")
 textFile.write("                     RESULTS BY COUNTY \n")
 textFile.write("----------------------------------------------------------\n")
-textFile.write("                       under dev.\n")
-textFile.close()
-    
-
+for k in range(len(county_list)):
+    textFile.write(f'COUNTY: {county_list[k]} \n')
+    textFile.write(f'Votes: {county_tot_votes[k]} ({round(county_tot_votes[k]/tot_vote_counter * 100, 3)}% of state total) \n\n')
+    k_tot = 0
+    for f in candidate_list:
+            k_tot= k_tot + county_candidates[county_list[k]][f]
+    for g in candidate_list:
+        textFile.write(f'{g}: {round(county_candidates[county_list[k]][g] / k_tot *100, 2)}% ({county_candidates[county_list[k]][g]} votes)\n')
+    textFile.write("\n----------------------------------------------------------\n")
+textFile.write("==========================================================\n")
 
